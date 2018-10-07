4179620b889f5c483f0605963b2a573bd609db75
Abby Tse <37357371+xabbirix@users.noreply.github.com>
2018-10-06 22:15:44 -0400

    Update run.py

--- run.py
+++ run.py
@@ -21,8 +21,8 @@ Q1 = """create table my_graph(
 # execute the statement 
 crsr.execute(Q1)
 
-print "Q1"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q1")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #insert values into the table
 Q2 = """insert into my_graph values (1,5,11), (2,11,2), (3,11,9), (4,11,10), (5,7,11), (6,7,8), (7,8,9),(8,3,8), (9,3,10);
@@ -30,8 +30,8 @@ Q2 = """insert into my_graph values (1,5,11), (2,11,2), (3,11,9), (4,11,10), (5,
 # execute the statement 
 crsr.execute(Q2)
 
-print "Q2"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q2")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #returns all distinct source ids 
 Q3 = """select distinct source_id 
@@ -39,8 +39,8 @@ Q3 = """select distinct source_id
 # execute the statement 
 crsr.execute(Q3)
 
-print "Q3"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q3")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #returns all link_ids where the source id is larger than destination id 
 Q4 = """select link_id
@@ -50,8 +50,8 @@ Q4 = """select link_id
 # execute the statement 
 crsr.execute(Q4)
 
-print "Q4"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q4")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #add weight column to each link_id
 Q5 = """alter table my_graph
@@ -60,8 +60,8 @@ Q5 = """alter table my_graph
 # execute the statement 
 crsr.execute(Q5)
 
-print "Q5"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q5")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #randomly assign weight
 Q6 = """update my_graph
@@ -70,8 +70,8 @@ Q6 = """update my_graph
 # execute the statement 
 crsr.execute(Q6)
 
-print "Q6"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q6")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #return the link with largest value of weight
 Q7 = """select link_id
@@ -81,8 +81,8 @@ Q7 = """select link_id
 # execute the statement 
 crsr.execute(Q7)
 
-print "Q7"
-print pd.read_sql_query("select * from my_graph", connection)
+print ("Q7")
+print (pd.read_sql_query("select * from my_graph", connection))
 
 #drop table
 Q8 = """drop table if exists my_graph;"""
